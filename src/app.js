4ec9624c4fb77b56d01c9053c5fbe40c8ea76f81
karithaha1 <karithaha1@gmail.com>
2025-11-18 03:15:14 +0700

    no message

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import routes from './routes/index.js';
+import cors from "cors";
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use(express.json());
+
+
 app.use("/uploads", express.static("uploads"));
 app.use('/api', routes);
 
